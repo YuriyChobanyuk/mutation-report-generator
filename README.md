f28f929bbb44dc87284145fab2fb15f802048728
Yurii Chobaniuk <ycho@softserveinc.com>
2022-11-29 11:24:24 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Action generates mutation html report
+
+This action consumes StrykerOutput folder placed in the root and aggregates results of mutation
+testing into a single report.html file. (Which later can be published or exposed as an artifact)
---
+++ index.js
@@ -0,0 +1,67 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const template = getTemplate();
+
+const aggregateMutationResults = async () => {
+  const directories = await getDirectories('./StrykerOutput');
+
+  for (let i = 0; i < directories.length; i++) {
+    const data = JSON.parse(await fs.readFile(getMutationResultPath(directories[i]), {encoding: 'utf8'}));
+
+    template.files = {
+      ...template.files,
+      ...data.files,
+    }
+  }
+
+  const reportContent = getHtmlContent(JSON.stringify(template));
+  await fs.writeFile(path.join(__dirname, './report.html'), reportContent, {encoding: 'utf8'});
+}
+
+aggregateMutationResults()
+  .catch(e => console.log(e));
+
+function getHtmlContent(reportData) {
+  return `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Mutation resutls</title>
+  </head>
+  <body>
+  <script src="https://www.unpkg.com/mutation-testing-elements@1.7.12/dist/mutation-test-elements.js"></script>
+    <mutation-test-report-app src="mutation-report.json">
+      Your browser does not support custom elements. Please use a modern browser.
+    </mutation-test-report-app>
+    <script>
+      document.querySelector('mutation-test-report-app').report = ${reportData};
+    </script>
+  </body>
+</html>
+`;
+}
+
+function getTemplate() {
+  return {
+    schemaVersion: "1",
+    thresholds: {
+      high: 80,
+      low: 60
+    },
+    projectRoot: "",
+    files: {}
+  }
+}
+
+function getMutationResultPath(dir) {
+  return path.join(__dirname, `./StrykerOutput/${dir}/reports/mutation-report.json`);
+}
+
+// Get list of directories where dotnet-stryker puts data per project
+async function getDirectories(source) {
+  return (await fs.readdir(path.join(__dirname, source), {withFileTypes: true}))
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name);
+}
